d2d369726f019f1bf6ada4ead4249aa867fb0e90
Tony Kruso <tony.kruso@gmail.com>
2019-07-11 21:18:48 -0400

    added bootswatch theme, added import under styles.scss

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 import './styles.scss';
-const Index = () => {
-  return (
-  <div id='home'>
-      <p className='welcome'>Welcome to React!</p>
-  </div>
-  )};
-ReactDOM.render(<Index />, document.getElementById('root'));
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
